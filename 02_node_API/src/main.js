64c4909dc3fba8e3e97ac8506eea5e3936ab97c5
Lucas Im <lkbzbcg15@gmail.com>
2021-10-24 22:16:10 +0900

    첫 커밋

---
+++ 02_node_API/src/main.js
@@ -0,0 +1,14 @@
+// @ts-check
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.end('hello!')
+})
+
+const PORT = 4000
+server.listen(PORT, () => {
+    console.log(`서버 실행중 포트 : ${PORT}`)
+})
+
+const x = null
